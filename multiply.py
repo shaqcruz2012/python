f35571fa4f7a6c85c9a08182b0f9fd4c2e5564d1
shaqc <shaq.cruz2012@gmail.com>
2023-01-25 23:13:34 -0500

    added multiply and testMultiply to python

---
+++ multiply.py
@@ -0,0 +1,8 @@
+# Write a function named multiply_3_numbers that 
+# takes three parameters, 
+# multiplies them together, and 
+# returns the result. 
+# Now write a test file for that function that contains at least 5 tests. Remember that the parameters could be ints or floats.
+
+def multiply_3_numbers(num1, num2, num3):
+return num1 * num2 * num3
---
+++ testMultiply.py
@@ -0,0 +1,6 @@
+#use with multiply
+print(multiply_3_numbers(2, 3, 4)) # Expected output: 24
+print(multiply_3_numbers(1.5, 2.5, 3)) # Expected output: 11.25
+print(multiply_3_numbers(-2, -3, 4)) # Expected output: 24
+print(multiply_3_numbers(1, 0, 1)) # Expected output: 0
+print(multiply_3_numbers(2.5, 3.5, 4.5)) # Expected output: 42.375
